b64c1ca213b35323fab62b97b7dba5193edd7bbb
IvyOwusu1 <ivyowusua13@gmail.com>
2026-07-23 07:58:10 +0000

    Add application configuration with pydantic-settings

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="TaskFlow Secure")
+from app.core.config import settings
+
+app = FastAPI(title=settings.APP_NAME)
+#app = FastAPI(title="TaskFlow Secure")
+# app = FastAPI(title=settings.APP_NAME, version=settings.APP_VERSION, debug=settings.DEBUG)
+
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to TaskFlow Secure ", 
-            "version": "0.1.0" ,
-            "status": "running"}
+    return {"APP_NAME": settings.APP_NAME, 
+            "APP_VERSION": settings.APP_VERSION ,
+            "status": "running",
+
+    }
+
+#return {"message": "Welcome to TaskFlow Secure ", 
+#            "version": "0.1.0" ,
+#            "status": "running"}
